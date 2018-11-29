5b260e7ab630c73011e8c6100e8e2f22b1c79b79
IlhamWahabiGX <wahabiputra@gmail.com>
2018-11-29 11:36:15 +0700

    Connect to mysql

--- webservice-bank/index.js
+++ webservice-bank/index.js
@@ -2,6 +2,18 @@ const express = require("express");
 const mysql = require("mysql");
 
 const app = express();
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "bank"
+});
+
+connection.connect();
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 
 app.listen(3000, () => {
   console.log("Server is listening at port 3000");
